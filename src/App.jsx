aeea52d714d1e7f424d36369262486d406979681
Ayoola <johnsonayoolaojo@gmail.com>
2026-04-08 11:23:17 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,56 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Search from './components/Search'
 
+const API_BASE_URL = 'https://api.themoviedb.org/3'
+
+const API_KEY = import.meta.env. VITE_TMB_API_KEY
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers : {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
+
+
 const App = () => {
+
     const [searchTerm,setSearchTerm] = useState('')
+    const[errorMessage,setErrorMessage] = useState('')
+    const [movieList, setMovieList] =  useState([])
+    const [isLoading, setIsLoading] = useState(false)
+     
+    const fetchMovies =  async () => {
+
+      setisLoading(true)
+      setErrorMessage('')
+
+      try {
+           const endpoint = `${API_BASE_URL}/discover/movie?sort_by = popularity.desc`
+           const response =  await fetch(endpoint,API_OPTIONS)
+
+            if(!response.ok) {
+          throw new Error('Failed to fetch Movies')
+         }
+
+         const data = await response.json()
+            if (data.Response === 'False') {
+                setErrorMessage(data.Error || 'Failed to get movies')
+                setMovieList([]);
+                return;
+              }
+      } catch (error) {
+         
+      }
+   
+   
+}
+
+
+     useEffect(()=>{
+       fetchMovies()
+     },[])
 
   return (
     <main>
@@ -14,6 +62,12 @@ const App = () => {
          </header>
          
          <Search searchTerm = {searchTerm}  setSearchTerm = {setSearchTerm}/>
+
+         <section className='all-movies'>
+          <h2>All movies</h2>
+          {errorMessage && <p className='text-red-500'>{errorMessage}</p>  }
+
+         </section>
       </div>
       
     </main>
